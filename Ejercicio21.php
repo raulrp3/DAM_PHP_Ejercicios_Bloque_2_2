c3a59dfe1d6fc9d2013248d6021ead6dda44ca4c
raulrp3 <raul.ramirez@escuelaestech.es>
2019-10-20 12:23:10 +0200

    Ejercicios 20-21

--- Ejercicio21.php
+++ Ejercicio21.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ejercicio 20</title>
+    <title>Ejercicio 21</title>
 </head>
 <body>
     <?php
@@ -12,8 +12,13 @@
         $animals = ['ballena.svg' => 'Ballena', 'caballito-mar.svg' => 'Caballito de mar', 'camello.svg' => 'Camello', 'cebra.svg' => 'Cebra', 'elefante.svg' => 'Elefante', 'hipopotamo.svg' => 'Hipopotamo', 'jirafa.svg' => 'Jirafa', 'leon.svg' => 'León', 'leopardo.svg' => 'Leopardo', 'medusa.svg' => 'Medusa', 'mono.svg' => 'Mono', 'oso-blanco.svg' => 'Oso polar', 'oso.svg' => 'Oso', 'pajaro.svg' => 'Pájaro', 'pinguino.svg' => 'Pingüino', 'rinoceronte.svg' => 'Rinoceronte', 'serpiente.svg' => 'Serpiente', 'tigre.svg' => 'Tigre', 'tortuga-marina.svg' => 'Tortuga marina', 'tortuga.svg' => 'Tortuga'];
         $file = fopen('animales.txt', 'w');
 
+        if (!file_exists('copiaseg')){
+            mkdir('copiaseg');
+        }
+
         while(($img = $images->read()) != false){
             if ($img != '.' && $img != '..'){
+                copy('img/'.$img, 'copiaseg/'.$img);
                 fwrite($file, $img.'('.filesize('img/'.$img).')'.PHP_EOL);
                 echo '<h4>'.$animals[$img].'</h4>';
                 echo '<img src=img/'.$img.'></img>';
